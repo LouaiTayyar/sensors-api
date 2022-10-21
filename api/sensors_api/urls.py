9809981e5dbed60a430be1e2d933b67259e7bde1
louaitayyar <louaitayyar@outlook.com>
2022-10-21 18:24:24 +0300

    + Heroku

--- api/sensors_api/urls.py
+++ api/sensors_api/urls.py
@@ -5,3 +5,6 @@ urlpatterns = [
 	path('sensors/', SensorsAPIView.as_view(), name='sensors'),
     path('readings/', ReadingsAPIView.as_view(), name='readings'),
 ]
+
+#    for next time, turn extra_info into end-points like so:
+#    path('readings/<str:metric>', ReadingsAPIView.as_view(), name='readings'),
