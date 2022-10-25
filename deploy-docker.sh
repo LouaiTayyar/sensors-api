dc59eb8ea00ef09308ae9714e35e242ae8c641bc
louaitayyar <louaitayyar@outlook.com>
2022-10-25 10:51:30 +0300

    complete

--- deploy-docker.sh
+++ deploy-docker.sh
@@ -1,5 +1,5 @@
 docker-compose up -d --build
-docker exec -it sensorsapi_api_1 python3 manage.py collectstatic --noinput
-docker exec -it sensorsapi_api_1 python3 manage.py makemigrations --noinput
-docker exec -it sensorsapi_api_1 python3 manage.py migrate --noinput
-docker exec -it sensorsapi_api_1 python3 manage.py test --noinput
+docker exec -it sensorsapi_api python3 manage.py collectstatic --noinput
+docker exec -it sensorsapi_api python3 manage.py makemigrations --noinput
+docker exec -it sensorsapi_api python3 manage.py migrate --noinput
+docker exec -it sensorsapi_api python3 manage.py test --noinput
