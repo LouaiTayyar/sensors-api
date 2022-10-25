79719a477c6b53eef6941aecc05f1374b8d15211
louaitayyar <louaitayyar@outlook.com>
2022-10-25 17:28:21 +0300

    complete

--- api/sensors_api/tests.py
+++ api/sensors_api/tests.py
@@ -1,14 +1,33 @@
+from venv import create
 from .models import Sensors,Readings
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
 from .serializers import SensorsSerializer, ReadingsSerializer
 
-class CreateSensorsTest(APITestCase):
+def create_test_sensor():
+    sensor = Sensors.objects.create(
+        sensor_id = 1,
+        type = 'Temperature Sensor',
+        vendor_name = 'sample name',
+        vendor_email = 'sample@email.com',
+        description = 'description',
+        location = 'location')
+    return sensor
 
-    def setUp(self):
-        self.url = reverse('sensors')
-        self.data = {
+def create_test_reading(sensor):
+    reading = Readings.objects.create(
+    reading_id = 1,
+    sensor = sensor,
+    type = 'type',
+    value = 'value',
+    date = '2022-10-11',
+    description = 'description',
+    time = 'time'
+    )    
+    return reading  
+
+test_sensor_data = {
             'sensor_id': 1,
             'type': 'Temperature Sensor',
             'vendor_name': 'sample name',
@@ -16,6 +35,21 @@ class CreateSensorsTest(APITestCase):
             'description': 'description',
             'location': 'location'
         }
+
+test_reading_data = {
+            'sensor': 1,
+            'type': 'type',
+            'value': 'value',
+            'date': '2022-10-11',
+            'description': 'description',
+            'time': 'time'
+        }
+
+class CreateSensorsTest(APITestCase):
+
+    def setUp(self):
+        self.url = reverse('sensors')
+        self.data = test_sensor_data
 
     def test_can_create_sensor(self):
         response = self.client.post(self.url, self.data)
@@ -25,13 +59,7 @@ class ReadSensorsTest(APITestCase):
 
     def setUp(self):
         self.url = reverse('sensors')
-        self.sensor = Sensors.objects.create(
-            sensor_id = 1,
-            type = 'Temperature Sensor',
-            vendor_name = 'sample name',
-            vendor_email = 'sample@email.com',
-            description = 'description',
-            location = 'location')
+        self.sensor = create_test_sensor()
         self.sensor_detail_url = self.url + '?sensor_id=1'
 
     def test_can_read_sensor_list(self):
@@ -47,13 +75,7 @@ class UpdateSensorsTest(APITestCase):
 
     def setUp(self):
         self.url = reverse('sensors')
-        self.sensor = Sensors.objects.create(
-            sensor_id = 1,
-            type = 'Temperature Sensor',
-            vendor_name = 'sample name',
-            vendor_email = 'sample@email.com',
-            description = 'description',
-            location = 'location')
+        self.sensor = create_test_sensor()
         self.sensor_detail_url = self.url + '?sensor_id=1'
         self.data = SensorsSerializer(self.sensor).data
         self.data.update({'type': 'Humidity Sensor'})
@@ -66,13 +88,7 @@ class DeleteSensorsTest(APITestCase):
 
     def setUp(self):
         self.url = reverse('sensors')
-        self.sensor = Sensors.objects.create(
-            sensor_id = 1,
-            type = 'Temperature Sensor',
-            vendor_name = 'sample name',
-            vendor_email = 'sample@email.com',
-            description = 'description',
-            location = 'location')
+        self.sensor = create_test_sensor()
         self.sensor_detail_url = self.url + '?sensor_id=1'
             
     def test_can_delete_sensor(self):
@@ -84,21 +100,8 @@ class CreateReadingsTest(APITestCase):
 
     def setUp(self):
         self.url = reverse('readings')
-        self.sensor = Sensors.objects.create(
-            type = 'Temperature Sensor',
-            vendor_name = 'sample name',
-            vendor_email = 'sample@email.com',
-            description = 'description',
-            location = 'location'
-        )
-        self.data = {
-            'sensor': 1,
-            'type': 'type',
-            'value': 'value',
-            'date': '2022-10-11',
-            'description': 'description',
-            'time': 'time'
-        }
+        self.sensor = create_test_sensor()
+        self.data = test_reading_data
 
     def test_can_create_reading(self):
         response = self.client.post(self.url, self.data)
@@ -108,22 +111,8 @@ class ReadReadingsTest(APITestCase):
 
     def setUp(self):
         self.url = reverse('readings')
-        self.sensor = Sensors.objects.create(
-            sensor_id = 1,
-            type = 'Temperature Sensor',
-            vendor_name = 'sample name',
-            vendor_email = 'sample@email.com',
-            description = 'description',
-            location = 'location')
-        self.reading = Readings.objects.create(
-            reading_id = 1,
-            sensor = self.sensor,
-            type = 'type',
-            value = 'value',
-            date = '2022-10-11',
-            description = 'description',
-            time = '12:00'
-        )        
+        self.sensor = create_test_sensor()
+        self.reading = create_test_reading(self.sensor)      
         self.reading_detail_url = self.url + '?sensor_type=Temperature Sensor&sensor_location=location&time=12:00'
 
     def test_can_read_reading_list(self):
@@ -139,22 +128,8 @@ class UpdateReadingsTest(APITestCase):
 
     def setUp(self):
         self.url = reverse('readings')
-        self.sensor = Sensors.objects.create(
-            sensor_id = 1,
-            type = 'Temperature Sensor',
-            vendor_name = 'sample name',
-            vendor_email = 'sample@email.com',
-            description = 'description',
-            location = 'location')
-        self.reading = Readings.objects.create(
-            reading_id = 1,
-            sensor = self.sensor,
-            type = 'type',
-            value = 'value',
-            date = '2022-10-11',
-            description = 'description',
-            time = 'time'
-        )        
+        self.sensor = create_test_sensor()
+        self.reading = create_test_reading(self.sensor)      
         self.reading_detail_url = self.url + '?reading_id=1'
         self.data = ReadingsSerializer(self.reading).data
         self.data.update({'type': 'updated type'})
@@ -167,22 +142,8 @@ class DeleteReadingsTest(APITestCase):
 
     def setUp(self):
         self.url = reverse('readings')
-        self.sensor = Sensors.objects.create(
-            sensor_id = 1,
-            type = 'Temperature Sensor',
-            vendor_name = 'sample name',
-            vendor_email = 'sample@email.com',
-            description = 'description',
-            location = 'location')
-        self.reading = Readings.objects.create(
-            reading_id = 1,
-            sensor = self.sensor,
-            type = 'type',
-            value = 'value',
-            date = '2022-10-11',
-            description = 'description',
-            time = 'time'
-        )        
+        self.sensor = create_test_sensor()
+        self.reading = create_test_reading(self.sensor)      
         self.reading_detail_url = self.url + '?reading_id=1'
             
     def test_can_delete_reading(self):
