9ee281fd128bf51c63cfd535b2269b17c1d82294
louaitayyar <louaitayyar@outlook.com>
2022-11-02 13:56:11 +0200

    finalizing pipeline

--- tools/setupjenkins.sh
+++ tools/setupjenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #docker run -d --name sonarqube -e SONAR_ES_BOOTSTRAPCHECKS_DISABLE=true -p 9000:9000 -p 9092:9092 sonarqube:latest
-
+#aaaa
 docker run -d --name=jenkins --privileged -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock -p 8080:8080 -p 50000:50000 --restart=on-failure jenkins/jenkins:lts-jdk11
 docker exec -it -u0 jenkins bash 
 curl https://get.docker.com/ > dockerinstall && chmod 777 dockerinstall && ./dockerinstall
