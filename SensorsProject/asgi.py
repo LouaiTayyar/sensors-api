01ed04546fba58596a437f2909856fd617fe935f
louaitayyar <louaitayyar@outlook.com>
2022-11-04 09:46:33 +0200

    Finished setting up Pipeline

--- SensorsProject/asgi.py
+++ SensorsProject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for SensorsAPI project.
+ASGI config for SensorsProject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SensorsAPI.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SensorsProject.settings")
 
 application = get_asgi_application()
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ chmod 777 /var/run/docker.sock
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
-gunicorn SensorsAPI.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn SensorsProject.wsgi:application --bind 0.0.0.0:$PORT
