adfd976f353eab966a9903ae71c3c1543d08a7d8
louaitayyar <louaitayyar@outlook.com>
2022-11-08 11:21:34 +0200

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,3 +4,5 @@ python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
 gunicorn SensorsProject.wsgi:application --bind 0.0.0.0:$PORT
+
+#test
