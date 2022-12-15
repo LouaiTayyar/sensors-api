58f37da772a780415ec1d64459a7ec38ccb80d00
Louai Tayyar <louai.tayyar@netcompany-intrasoft.com>
2022-12-15 13:39:28 +0200

    Update deploy-heroku.sh

--- tools/deploy-heroku.sh
+++ tools/deploy-heroku.sh
@@ -4,7 +4,6 @@
 #create heroku app
 #heroku create sensors-readings
 heroku container:login
-docker login --username=louaitayyar@outlook.com --password=0b6f1966-8fb7-48bf-9118-5f44067dd66e registry.heroku.com
 docker build -t registry.heroku.com/sensors-readings/web .
 docker push registry.heroku.com/sensors-readings/web 
 heroku container:release web -a sensors-readings
